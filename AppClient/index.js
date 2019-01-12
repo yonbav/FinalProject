13a30dbc5fa5c0669930e5c961cb338af9c5c4f1
yonbav <yonbav@gmail.com>
2019-01-12 20:54:09 +0200

    Test Removing all IDEA warnings

--- AppClient/index.js
+++ AppClient/index.js
@@ -1,4 +1,4 @@
-/** @format */
+/** @format **/
 
 import {AppRegistry, Text, View} from 'react-native';
 import React from 'react';
@@ -6,13 +6,8 @@ import { Provider } from 'react-redux';
 import {createStore,applyMiddleware} from 'redux';
 import reducers from './src/reducers/reducers'
 import {YellowBox} from 'react-native';
-import LoginForm from "./src/Components/LoginForm";
 import  ReduxThunk from 'redux-thunk';
 import RouterComp from "./src/RouterComp";
-YellowBox.ignoreWarnings([
-    'Remote debugger',
-    console.disableYellowBox = true
-])
 
 const App = () => (
     <Provider store={createStore(reducers,{},applyMiddleware(ReduxThunk))}>
